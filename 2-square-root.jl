a8a26b14479429a511bebd6399b3bf621b66ef5f
camilasbraz <camilabraz03@gmail.com>
2023-06-20 00:18:25 -0300

    primeiros 2 exercicios

---
+++ 2-square-root.jl
@@ -0,0 +1,20 @@
+
+# https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
+# Babylonian method
+function square_root(n)
+    if n <= 0
+        error("DomainError")
+    else 
+        attempt = n/2
+        while true
+            attempt_aux = (attempt + n/attempt)/2
+            if abs(attempt_aux - attempt) < 0.000001
+                return(round(attempt))
+            end
+            attempt = attempt_aux
+        end
+    end
+end
+
+s = rand(1:isqrt(typemax(Int64)))
+square_root(s^2)
